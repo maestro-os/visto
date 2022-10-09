952c2274dcd555ed28571e9892d95e302383ec4d
llenotre <llenotre@student.42.fr>
2022-10-10 00:55:42 +0200

    Screens listing WIP

--- src/drm.rs
+++ src/drm.rs
@@ -42,35 +42,82 @@ macro_rules! iowr {
 const DRM_IOCTL_BASE: u64 = b'd' as u64;
 /// DRM ioctl command: TODO doc
 const DRM_IOCTL_MODE_GETRESOURCES: u64 = iowr!(DRM_IOCTL_BASE, 0xa0, DRMModeCardRes);
+/// DRM ioctl command: TODO doc
+const DRM_IOCTL_MODE_GETCONNECTOR: u64 = iowr!(DRM_IOCTL_BASE, 0xa7, DRMModeGetConnector);
 
 /// TODO doc
 #[derive(Debug, Default)]
 #[repr(C)]
 struct DRMModeCardRes {
-	/// TODO doc
+	/// The pointer to the framebuffers IDs list.
 	fb_id_ptr: u64,
-	/// TODO doc
+	/// The pointer to the CRTCs IDs list.
 	crtc_id_ptr: u64,
-	/// TODO doc
+	/// The pointer to the connectors IDs list.
 	connector_id_ptr: u64,
-	/// TODO doc
+	/// The pointer to the encoders IDs list.
 	encoder_id_ptr: u64,
-	/// TODO doc
+	/// The number of framebuffers available.
 	count_fbs: u32,
-	/// TODO doc
+	/// The number of CRTCs available.
 	count_crtcs: u32,
-	/// TODO doc
+	/// The number of connectors available.
 	count_connectors: u32,
-	/// TODO doc
+	/// The number of encoders available.
 	count_encoders: u32,
-	/// TODO doc
+	/// The minimum width of a framebuffer.
 	min_width: u32,
-	/// TODO doc
+	/// The minimum height of a framebuffer.
 	max_width: u32,
-	/// TODO doc
+	/// The maximum width of a framebuffer.
 	min_height: u32,
-	/// TODO doc
+	/// The maximum height of a framebuffer.
 	max_height: u32,
+}
+
+/// TODO doc
+#[derive(Debug, Default)]
+#[repr(C)]
+struct DRMModeGetConnector {
+	/// Pointer to array of object IDs.
+	encoders_ptr: u64,
+	/// Pointer to struct DRMModeModeinfo array.
+	modes_ptr: u64,
+	/// Pointer to array of property IDs.
+	props_ptr: u64,
+	/// Pointer to array of property values.
+	prop_values_ptr: u64,
+
+	/// Number of modes.
+	count_modes: u32,
+	/// Number of properties.
+	count_props: u32,
+	/// Number of encoders.
+	count_encoders: u32,
+
+	/// Object ID of the current encoder.
+	encoder_id: u32,
+	/// Object ID of the connector.
+	connector_id: u32,
+	/// Type of the connector.
+	connector_type: u32,
+	/// Type-specific connector number.
+	/// 
+	/// This is not an object ID. This is a per-type connector number. Each (type, type_id)
+	/// combination is unique across all connectors of a DRM device.
+	connector_type_id: u32,
+
+	/// Status of the connector.
+	connection: u32,
+	/// Width of the connected sink in millimeters.
+	mm_width: u32,
+	/// Height of the connected sink in millimeters.
+	mm_height: u32,
+	/// Subpixel order of the connected sink.
+	subpixel: u32,
+
+	/// Padding, must be zero.
+	pad: u32,
 }
 
 /// Structure representing a DRI device.
@@ -79,15 +126,36 @@ pub struct DRICard {
 	path: String,
 	/// The open device file.
 	dev: File,
+
+	/// The minimum width of a framebuffer.
+	fb_min_width: u32,
+	/// The maximum width of a framebuffer.
+	fb_max_width: u32,
+	/// The minimum height of a framebuffer.
+	fb_min_height: u32,
+	/// The maximum height of a framebuffer.
+	fb_max_height: u32,
+
+	/// List of framebuffer IDs.
+	fb_id_ptr: Vec<u32>,
+	/// List of CRTC IDs.
+	crtc_id_ptr: Vec<u32>,
+	/// List of connectors IDs.
+	connector_id_ptr: Vec<u32>,
+	/// List of encoders IDs.
+	encoder_id_ptr: Vec<u32>,
 }
 
-/// Scan DRI's devices and returns a list of available devices.
-pub fn scan_devices() -> Vec<DRICard> {
-	let mut devs = vec![];
+impl DRICard {
+	/// Loads the device with ID `id`. If the device doesn't exist, the function returns None.
+	pub fn load(id: usize) -> Option<Self> {
+		let path = format!("/dev/dri/card{}", id);
 
-	for i in 0..16 {
-		let path = format!("/dev/dri/card{}", i);
-		if let Ok(dev) = File::open(&path) {
+		loop {
+			let dev = match File::open(&path) {
+				Ok(dev) => dev,
+				Err(_) => return None,
+			};
 			let fd = dev.as_raw_fd();
 
 			let mut card_res = DRMModeCardRes::default();
@@ -98,34 +166,69 @@ pub fn scan_devices() -> Vec<DRICard> {
 					&mut card_res as *const _
 				)
 			};
-
-			// On error, ignore the device and continue iteration
 			if res < 0 {
-				continue;
+				return None;
 			}
 
-			// TODO rm
-			println!("-> {} {} {:?}", res, std::io::Error::last_os_error(), card_res);
-
-			// TODO Allocate buffers for:
-			// - fb_id_ptr
-			// - crtc_id_ptr
-			// - connector_id_ptr
-			// - encoder_id_ptr
-			// TODO Get connectors list
-			// TODO Iterate on encoders available for each connectors
-			// TODO Associate a CRTC for each encoder
-
-			// TODO List valid modes for each connectors
-			// TODO Select a mode for each connector
-			// TODO Create framebuffers
-
-			devs.push(DRICard {
-				path,
+			let card = Self {
+				path: path.clone(),
 				dev,
-			});
+
+				fb_min_width: card_res.min_width,
+				fb_max_width: card_res.max_width,
+				fb_min_height: card_res.min_height,
+				fb_max_height: card_res.max_height,
+
+				fb_id_ptr: vec![0; card_res.count_fbs as usize],
+				crtc_id_ptr: vec![0; card_res.count_crtcs as usize],
+				connector_id_ptr: vec![0; card_res.count_connectors as usize],
+				encoder_id_ptr: vec![0; card_res.count_encoders as usize],
+			};
+
+			card_res.fb_id_ptr = card.fb_id_ptr.as_ptr() as _;
+			card_res.crtc_id_ptr = card.crtc_id_ptr.as_ptr() as _;
+			card_res.connector_id_ptr = card.connector_id_ptr.as_ptr() as _;
+			card_res.encoder_id_ptr = card.encoder_id_ptr.as_ptr() as _;
+
+			let res = unsafe {
+				libc::ioctl(
+					fd,
+					DRM_IOCTL_MODE_GETRESOURCES,
+					&mut card_res as *const _
+				)
+			};
+			if res >= 0 {
+				return Some(card);
+			}
 		}
 	}
 
-	devs
+	/// Scan DRI's devices and returns a list of available devices.
+	pub fn scan() -> Vec<Self> {
+		let mut devs = vec![];
+
+		for i in 0..16 {
+			if let Some(dev) = Self::load(i) {
+				devs.push(dev);
+			}
+		}
+
+		devs
+	}
+
+	/// Returns the list of connectors associated with the device.
+	pub fn get_connectors(&mut self) {
+		let mut _conn = DRMModeGetConnector::default();
+
+		// TODO
+		todo!();
+	}
+
+	// TODO Rest of init
+	// - Get connectors list
+	// - Iterate on encoders available for each connectors
+	// - Associate a CRTC for each encoder
+	// - List valid modes for each connectors
+	// - Select a mode for each connector
+	// - Create framebuffers
 }
